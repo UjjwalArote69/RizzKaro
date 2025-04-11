9e6c7c52810df828a5eaebda94cd26fa10d5231a
UjjwalArote69 <155384273+UjjwalArote69@users.noreply.github.com>
2025-04-11 20:20:30 +0530

    Create corsOption.js

---
+++ Backend/config/corsOption.js
@@ -0,0 +1,17 @@
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://rizz-karo.vercel.app'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+module.exports = corsOptions;

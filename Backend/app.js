8316a94daafca1c11e27a43f12aa4c3fac573e39
UjjwalArote69 <155384273+UjjwalArote69@users.noreply.github.com>
2025-04-11 20:21:12 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -3,18 +3,14 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/user.route");
 const replyRoutes = require("./routes/reply.route");
+const corsOptions = require('./config/corsOptions');
 
 connectDB();
 
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/reply", replyRoutes);

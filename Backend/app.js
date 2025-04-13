d085f44db134fce145f86c5efbe594dcdc21bc91
UjjwalArote69 <155384273+UjjwalArote69@users.noreply.github.com>
2025-04-13 13:24:45 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -24,7 +24,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors(corsOptions()));
 app.use('/public', express.static('public'));
 
 // Routes

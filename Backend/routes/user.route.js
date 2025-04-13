45483b116f35781e9a3ebed1d9c97860ee31e129
UjjwalArote69 <155384273+UjjwalArote69@users.noreply.github.com>
2025-04-13 12:40:59 +0530

    Update user.route.js

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-// const { body } = require('express-validator')
+const upload = require("../config/multer");
 const userController = require("../controllers/user.controller");
 const authMiddleware = require("../middleware/auth.middleware");
+const multer = require("multer");
+// const upload = multer({ dest: '/' });
 
 router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
@@ -15,10 +17,31 @@ router.get("/protected", authMiddleware.authUser, (req, res) => {
 });
 
 router.put("/follow/:id", authMiddleware.authUser, userController.followUser);
-router.put("/unfollow/:id", authMiddleware.authUser, userController.unfollowUser);
-router.get("/profile/:username", authMiddleware.authUser, userController.getUserProfile);
-
-// router.get("/profile", authMiddleware.authUser, userController.getUserProfile);
-
+router.put(
+  "/unfollow/:id",
+  authMiddleware.authUser,
+  userController.unfollowUser
+);
+router.get(
+  "/profile/:username",
+  authMiddleware.authUser,
+  userController.getUserProfile
+);
+router.put(
+  "/update-profile",
+  authMiddleware.authUser,
+  userController.updateUserProfile
+);
+router.put(
+  "/update-password",
+  authMiddleware.authUser,
+  userController.updatePassword
+);
+router.put(
+  "/avatar",
+  authMiddleware.authUser,
+  upload.single("avatar"),
+  userController.updateAvatar,
+);
 
 module.exports = router;

c5d688f3a431ad30cde641c7c4ba4a3a94376b2e
UjjwalArote69 <155384273+UjjwalArote69@users.noreply.github.com>
2025-04-13 13:05:00 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/user.route");
 const replyRoutes = require("./routes/reply.route");
 const postRoutes = require("./routes/post.route");
+const corsOptions = require("./config/corsOptions");
 
 // Add this at the top of your server file (app.js or server.js)
 const fs = require('fs');
@@ -23,9 +24,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors(corsOption)
-);
+app.use(cors(corsOption));
 app.use('/public', express.static('public'));
 
 // Routes

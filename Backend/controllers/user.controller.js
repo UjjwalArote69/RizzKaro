5f4d470c8def5bc523b0751e2a8406cc99004535
Ujjwal Arote <155384273+UjjwalArote69@users.noreply.github.com>
2025-07-14 12:10:48 +0530

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -32,7 +32,7 @@ module.exports.registerUser = async (req, res) => {
         username: newUser.username,
         fullname: newUser.fullname,
         email: newUser.email,
-        token: jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "7d" }),
+        token,
       },
     });
   } catch (error) {
@@ -78,7 +78,7 @@ module.exports.loginUser = async (req, res) => {
         fullname: user.fullname,
         username: user.username,
         email: user.email,
-        token: jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "7d" }),
+        token,
       },
     });
   } catch (err) {
